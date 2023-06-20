f2f73d5ad8cc610b0bda1b949799ea3e3caac0e6
Michael Rajotte <michael.rajotte@gmail.com>
2023-06-20 18:01:22 +0200

    40 pixel ring fixes

--- source/ProtonPack/Configuration.h
+++ source/ProtonPack/Configuration.h
@@ -28,7 +28,7 @@
  *  25 LEDs in the stock Haslab kit. 13 in the Powercell and 12 in the Cyclotron lid. 
  *  
  *  For the stock Haslab LEDs, use const int i_pack_num_leds = 25; and const i_1984_cyclotron_leds 1, 4, 7, 10.
- *  For a 40 LED NeoPixel ring, use const int i_pack_num_leds = 53 and const i_1984_cyclotron_leds 1, 11, 19, 29.
+ *  For a 40 LED NeoPixel ring, if you align your ring so that the first led is the middle, then use const int i_pack_num_leds = 53 and const i_1984_cyclotron_leds 0, 10, 18, 28.
  *  Adjust as neccesary depending on how you align your NeoPixel ring.
  *  You can use any LED setup with up to 40 LEDs. If you change them out to individual NeoPixels or NeoPixel Rings, adjust your settings accordingly.  
  *
@@ -42,7 +42,7 @@ const uint8_t i_1984_cyclotron_leds[4] = { 1, 4, 7, 10 };
 
 // For a 40 LED NeoPixel ring.
 //const uint8_t i_pack_num_leds = 53;
-//const uint8_t i_1984_cyclotron_leds[4] = { 1, 11, 19, 29};
+//const uint8_t i_1984_cyclotron_leds[4] = { 0, 10, 18, 28 };
 
 /*
  * Cyclotron Lid LED delays.
