6224e2db30fdff0e18e98382fb669409ce3161d2
Michael Rajotte <michael.rajotte@gmail.com>
2024-03-18 15:19:29 +0100

    music playback fixes

--- source/NeutronaWand/Actions.h
+++ source/NeutronaWand/Actions.h
@@ -1144,21 +1144,25 @@ void checkWandAction() {
         // Music track loop setting.
         if(WAND_MENU_LEVEL == MENU_LEVEL_1) {
           if(switch_intensify.isPressed()) {
-            if(i_music_count > 0) {
-              if(b_repeat_track == false) {
-                // Loop the track.
-                b_repeat_track = true;
+            if(b_repeat_track == false) {
+              // Loop the track.
+              b_repeat_track = true;
+              
+              if(i_music_count > 0) {
                 w_trig.trackLoop(i_current_music_track, 1);
               }
-              else {
-                b_repeat_track = false;
+            }
+            else {
+              b_repeat_track = false;
+
+              if(i_music_count > 0) {
                 w_trig.trackLoop(i_current_music_track, 0);
               }
             }
-
-            // Tell pack to loop the music track.
-            wandSerialSend(W_MUSIC_TRACK_LOOP_TOGGLE);
           }
+
+          // Tell pack to loop the music track.
+          wandSerialSend(W_MUSIC_TRACK_LOOP_TOGGLE);
         }
         else if(WAND_MENU_LEVEL == MENU_LEVEL_2) {
           if(switch_intensify.isPressed()) {
@@ -1327,7 +1331,7 @@ void checkWandAction() {
               if(b_playing_music == true) {
                 // Tell the pack to stop music.
                 wandSerialSend(W_MUSIC_STOP);
-
+                
                 if(b_gpstar_benchtest == true) {
                   stopMusic();
                 }
