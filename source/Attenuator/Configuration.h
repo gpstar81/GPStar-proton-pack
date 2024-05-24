f0da2a9ae514da16b22a8d90dc4b3f079c820620
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-05-23 20:02:35 -0400

    Fixing UI crap

--- source/Attenuator/Configuration.h
+++ source/Attenuator/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240523185133";
+String build_date = "20240523195331";
 
 /*
  * Preferred WiFi Network Defaults
