a610b8b8010877ed64a387d57399cf33f79b0daf
Michael Rajotte <michael.rajotte@gmail.com>
2024-05-27 12:16:35 +0200

    fix bug related to double voice played during the year changing to system default in the eeprom configuration menu

--- source/Attenuator/Configuration.h
+++ source/Attenuator/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240524143521";
+String build_date = "20240527121311";
 
 /*
  * Preferred WiFi Network Defaults
