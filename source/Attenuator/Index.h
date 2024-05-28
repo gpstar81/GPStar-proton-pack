497c823accfe7c5f4e5f9ba0392322dd6e185513
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-05-27 21:55:02 -0400

    Update Index.h

--- source/Attenuator/Index.h
+++ source/Attenuator/Index.h
@@ -143,7 +143,7 @@ const char INDEX_page[] PROGMEM = R"=====(
 
   <div id="tab4" class="tab">
     <div class="card" style="text-align:center;">
-  	<a href="/settings/attenuator">Attenuator Settings</a>
+  	  <a href="/settings/attenuator">Attenuator Settings</a>
       <br/>
       <br/>
       <a href="/settings/pack">Proton Pack Settings</a>
