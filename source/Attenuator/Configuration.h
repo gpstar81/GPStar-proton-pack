7cc4e41f21edaa791b53d11cd2e161307d8f3e5f
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-05-28 20:25:08 -0400

    Update from dev branch

--- source/Attenuator/Configuration.h
+++ source/Attenuator/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240528190408";
+String build_date = "20240528202203";
 
 /*
  * Preferred WiFi Network Defaults
