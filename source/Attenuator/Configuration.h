de4663f2fdec4948f5765ec070108c42668e81fa
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-06-09 09:47:05 -0400

    Rebuild binaries

--- source/Attenuator/Configuration.h
+++ source/Attenuator/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240604174331";
+String build_date = "20240609093309";
 
 /*
  * Preferred WiFi Network Defaults
