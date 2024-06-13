ac9105816ea358f79f99bf2927be84dedcb2bf8f
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-06-12 22:13:43 -0400

    Update Animations.h

--- source/Animations/Animations.h
+++ source/Animations/Animations.h
@@ -16,6 +16,7 @@ struct RingParams {
   RAMP_STATES rampState = NO_RAMP;    // Current ramping state
   uint8_t currentLED = 0;             // Current LED index
   uint8_t previousLED = 0;            // Previous LED index
+  uint8_t nextLED = 0;                // Next LED index
 };
 
 // Function to animate the LED ring
@@ -31,24 +32,29 @@ bool animateRing(RingParams &ring) {
   // Check if the delay has timed out
   if (ring.ledTimer.justFinished()) {
     if(ring.deviceName != NO_DEVICE) {
-      // Turn off the previous pixel but only if not on the first iteration around the ring.
+      // Turn off the previous pixel but only if not on the first iteration around the ring
       if(ring.previousLED != ring.currentLED) {
         ring.ledsArray[ring.previousLED] = CRGB::Black;
       }
 
-      // Turn on the current LED using the color (with saturation and brightness) as specified
-      ring.ledsArray[ring.currentLED] = ring.ledColor;
+      // Set the current LED to a portion of its current brightness for a tail effect
+      //uint8_t currentBrightness = ring.ledsArray[ring.currentLED].getAverageLight();
+      ring.ledsArray[ring.currentLED] = blend(ring.ledColor, CRGB::Black, 128);
+
+      // Turn on the next LED using the color (with saturation and brightness) as specified
+      ring.ledsArray[ring.nextLED] = ring.ledColor;
 
       // Show changes to the LEDs
       FastLED.show();
 
       // Move to the next LED in the ring (starting over as necessary)
       ring.previousLED = ring.currentLED; // Remember current as the previous LED
-      ring.currentLED = (ring.currentLED + 1) % ring.numLEDs; // Set the next LED
+      ring.currentLED = ring.nextLED; // Remember current as the previous LED
+      ring.nextLED = (ring.currentLED + 1) % ring.numLEDs; // Set the next LED
     }
 
-    // Restart the delay
-    ring.ledTimer.repeat();
+    // Restart the delay in case the value changed
+    ring.ledTimer.start(delayTime);
 
     if(ring.currentLED == 0 && ring.currentLED != ring.previousLED) {
       return true; // Returned to position 0, meaning a full revolution completed
