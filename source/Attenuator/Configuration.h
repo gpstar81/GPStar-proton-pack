9a1df7385f99770744dfce4260e092cf1e073ad2
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-06-15 16:53:13 -0400

    Update STL, rebuild binaries

--- source/Attenuator/Configuration.h
+++ source/Attenuator/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240612194319";
+String build_date = "20240615161908";
 
 /*
  * Preferred WiFi Network Defaults
