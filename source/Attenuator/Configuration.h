ba312d2769978acf91a173a7c530c25fb6d1fff7
Michael Rajotte <michael.rajotte@gmail.com>
2024-06-27 01:04:17 +0200

    rebuilt binaries

--- source/Attenuator/Configuration.h
+++ source/Attenuator/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240623144238";
+String build_date = "20240627010235";
 
 /*
  * Preferred WiFi Network Defaults
