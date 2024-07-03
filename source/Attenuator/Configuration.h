45226ed1fb0c877bfb41206fe21615d32da89f6b
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-07-02 21:39:33 -0400

    ESP32 Cleanup
    Restructures code for ESP32 for easier maintenance, moving all web-related code into the WebHandler.h; Removes unnecessary includes which are already included by other libraries

--- source/Attenuator/Configuration.h
+++ source/Attenuator/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240629172844";
+String build_date = "20240702213542";
 
 /*
  * Preferred WiFi Network Defaults
