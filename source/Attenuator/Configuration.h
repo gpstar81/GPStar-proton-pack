834ec8bdd8e2ec75c0adb4f03e5887cc07e113f3
nomakewan <nomakewan@users.noreply.github.com>
2024-07-10 22:57:39 -0700

    Recompile binaries

--- source/Attenuator/Configuration.h
+++ source/Attenuator/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240710075847";
+String build_date = "20240710225234";
 
 /*
  * Preferred WiFi Network Defaults
