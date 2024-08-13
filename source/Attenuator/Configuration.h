0d3f0ab5e1ecd4ac8c0ac4ffa1b78537d878b974
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-08-13 18:52:48 -0400

    Adjust freeze-up sound with new shutdown segment, add voices for inner panel options

--- source/Attenuator/Configuration.h
+++ source/Attenuator/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240811082513";
+String build_date = "20240813184137";
 
 /*
  * Preferred WiFi Network Defaults
