2762f66dca7de3138e61fe47953c7ad6e6e7c880
nomakewan <nomakewan@users.noreply.github.com>
2024-08-13 18:15:30 -0700

    Add missing sound defs to wand

--- source/NeutronaWand/MusicSounds.h
+++ source/NeutronaWand/MusicSounds.h
@@ -425,10 +425,12 @@ enum sound_fx {
   S_GB1_1984_WAND_BARREL_EXTEND,
   S_GB1_1984_BOOT_UP_ALT,
   S_FROZEN_EMPIRE_PACK_FREEZE_STOP,
-  S_FROZEN_EMPIRE_SHUTDOWN
+  S_FROZEN_EMPIRE_SHUTDOWN,
+  S_VOICE_INNER_CYCLOTRON_LED_PANEL_STATIC_COLORS,
+  S_VOICE_INNER_CYCLOTRON_LED_PANEL_DYNAMIC_COLORS
 };
 
 /*
  * Need to keep track which is the last sound effect, so we can iterate over the effects to adjust the volume gain on them.
  */
-const uint16_t i_last_effects_track = S_FROZEN_EMPIRE_SHUTDOWN;
+const uint16_t i_last_effects_track = S_VOICE_INNER_CYCLOTRON_LED_PANEL_DYNAMIC_COLORS;
