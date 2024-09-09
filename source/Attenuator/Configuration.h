86996e251f3f07615fa78c599f86154cf1124178
nomakewan <nomakewan@users.noreply.github.com>
2024-09-08 23:28:45 -0700

    FastLED 3.7.6 Update
    Updates the compile test to use latest instead of forcing FastLED 3.7.4, updates all binaries to use FastLED 3.7.6.

--- source/Attenuator/Configuration.h
+++ source/Attenuator/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240905172729";
+String build_date = "20240908232155";
 
 /*
  * Preferred WiFi Network Defaults
