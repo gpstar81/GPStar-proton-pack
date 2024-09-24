b10962b0c3cf522e26f50044aab842985d66fb92
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-09-24 09:54:06 -0400

    Correct libraries for ESP32, rebuild binaries

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240924094832";
+String build_date = "20240924095225";
 
 /*
  * Preferred WiFi Network Defaults
