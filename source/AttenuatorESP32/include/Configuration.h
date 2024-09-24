d3643177dc67c1ede7029c5852d71dc29bcd75b7
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-09-24 15:44:00 -0400

    Restore encoder button behavior

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240924152536";
+String build_date = "20240924154124";
 
 /*
  * Preferred WiFi Network Defaults
