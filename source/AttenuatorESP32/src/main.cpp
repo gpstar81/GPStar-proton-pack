236420c45b2dcc676778d7196944faf34a800794
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-10-25 13:56:58 -0400

    Move setup of preferences to new 1-time task

--- source/AttenuatorESP32/src/main.cpp
+++ source/AttenuatorESP32/src/main.cpp
@@ -51,9 +51,10 @@
 #include "System.h"
 
 // Task Handles
+TaskHandle_t AnimationTaskHandle = NULL;
+TaskHandle_t PreferencesTaskHandle = NULL;
 TaskHandle_t SerialCommsTaskHandle = NULL;
 TaskHandle_t UserInputTaskHandle = NULL;
-TaskHandle_t AnimationTaskHandle = NULL;
 TaskHandle_t WiFiManagementTaskHandle = NULL;
 TaskHandle_t WiFiSetupTaskHandle = NULL;
 
@@ -82,87 +83,27 @@ void idleTaskCore1(void * parameter) {
 }
 #endif
 
-// Serial Comms Task (Loop)
-void SerialCommsTask(void *parameter) {
-  #if defined(DEBUG_TASK_TO_CONSOLE)
-    // Confirm the core in use for this task, and when it runs.
-    Serial.print(F("Executing SerialCommsTask in core"));
-    Serial.print(xPortGetCoreID());
-    // Get the stack high water mark for optimizing bytes allocated.
-    Serial.print(F(" | Stack HWM: "));
-    Serial.println(uxTaskGetStackHighWaterMark(NULL));
-  #endif
+// Obtain a list of partitions for this device.
+void printPartitions() {
+  const esp_partition_t *partition;
+  esp_partition_iterator_t iterator = esp_partition_find(ESP_PARTITION_TYPE_ANY, ESP_PARTITION_SUBTYPE_ANY, NULL);
 
-  while(true) {
-    if(b_wait_for_pack) {
-      if(ms_packsync.justFinished()) {
-        // Tell the pack we are trying to sync.
-        attenuatorSerialSend(A_SYNC_START);
-
-        // Keep the on-board LED dark until sync'd.
-        digitalWrite(BUILT_IN_LED, LOW);
-
-        // Pause and try again in a moment.
-        ms_packsync.start(i_sync_initial_delay);
-      }
-
-      checkPack();
-
-      if(!b_wait_for_pack) {
-        // Indicate that we are no longer waiting on the pack.
-        digitalWrite(BUILT_IN_LED, HIGH);
-      }
-    }
-    else {
-      bool b_notify = checkPack(); // Always updates on pack check.
-
-      // If at any point this flag is true, we have comms open to the pack.
-      // This gets reset upon every bootup (read: re-connection to a pack).
-      if(b_notify) {
-        b_comms_open = true;
-      }
-
-      if(ms_packsync.justFinished()) {
-        // The pack just went missing, so treat as disconnected.
-        b_wait_for_pack = true;
-        ms_packsync.start(i_sync_initial_delay);
-      }
-
-      /**
-       * Alert any WebSocket clients after an API call was received.
-       *
-       * Note: We only perform this action if we have data from the pack
-       * which resulted in a significant state change--this prevents the
-       * device from spamming any downstream clients with unchanged data.
-       */
-      if(b_notify) {
-        notifyWSClients(); // Send latest status to the WebSocket.
-      }
-    }
-
-    vTaskDelay(2 / portTICK_PERIOD_MS); // 2ms delay
+  if (iterator == nullptr) {
+    Serial.println(F("No partitions found."));
+    return;
   }
-}
 
-// User Input Task (Loop)
-void UserInputTask(void *parameter) {
-  while(true) {
-    #if defined(DEBUG_TASK_TO_CONSOLE)
-      // Confirm the core in use for this task, and when it runs.
-      Serial.print(F("Executing UserInputTask in core"));
-      Serial.print(xPortGetCoreID());
-      // Get the stack high water mark for optimizing bytes allocated.
-      Serial.print(F(" | Stack HWM: "));
-      Serial.println(uxTaskGetStackHighWaterMark(NULL));
-    #endif
-
-    if(!b_wait_for_pack) {
-      // When not waiting for the pack go directly to checking user inputs.
-      checkUserInputs();
-    }
-
-    vTaskDelay(14 / portTICK_PERIOD_MS); // 14ms delay
+  Serial.println(F("Partitions:"));
+  while (iterator != nullptr) {
+    partition = esp_partition_get(iterator);
+    Serial.printf("Label: %s, Size: %u bytes, Address: 0x%08X\n",
+                  partition->label,
+                  partition->size,
+                  partition->address);
+    iterator = esp_partition_next(iterator);
   }
+
+  esp_partition_iterator_release(iterator);  // Release the iterator once done
 }
 
 // Animation Task (Loop)
@@ -206,184 +147,13 @@ void AnimationTask(void *parameter) {
   }
 }
 
-// WiFi Management Task (Loop)
-void WiFiManagementTask(void *parameter) {
-  while(true) {
-    #if defined(DEBUG_TASK_TO_CONSOLE)
-      // Confirm the core in use for this task, and when it runs.
-      Serial.print(F("Executing WiFiManagementTask in core"));
-      Serial.print(xPortGetCoreID());
-      // Get the stack high water mark for optimizing bytes allocated.
-      Serial.print(F(" | Stack HWM: "));
-      Serial.println(uxTaskGetStackHighWaterMark(NULL));
-    #endif
-
-    // Proceed with management if the AP and web server are started.
-    if(b_ap_started && b_ws_started) {
-      if(ms_cleanup.remaining() < 1) {
-        // Clean up oldest WebSocket connections.
-        ws.cleanupClients();
-
-        // Restart timer for next cleanup action.
-        ms_cleanup.start(i_websocketCleanup);
-      }
-
-      if(ms_cleanup.remaining() < 1) {
-        // Update the current count of AP clients.
-        i_ap_client_count = WiFi.softAPgetStationNum();
-
-        // Restart timer for next count.
-        ms_apclient.start(i_apClientCount);
-      }
-
-      if(ms_cleanup.remaining() < 1) {
-        // Handles device reboot after an OTA update.
-        ElegantOTA.loop();
-
-        // Restart timer for next check.
-        ms_otacheck.start(i_otaCheck);
-      }
-    }
-
-    vTaskDelay(100 / portTICK_PERIOD_MS); // 100ms delay
-  }
-}
-
-// WiFi Setup Task (Single-Run)
-void WiFiSetupTask(void *parameter) {
+// Preferences Task (Single-Run)
+void PreferencesTask(void *parameter) {
   #if defined(DEBUG_TASK_TO_CONSOLE)
     // Confirm the core in use for this task, and when it runs.
-    Serial.print(F("Executing WiFiSetupTask in core"));
+    Serial.print(F("Executing PreferencesTask in core"));
     Serial.println(xPortGetCoreID());
   #endif
-
-  // Begin by setting up WiFi as a prerequisite to all else.
-  if(startWiFi()) {
-    // Start the local web server.
-    startWebServer();
-
-    // Begin timer for remote client events.
-    ms_cleanup.start(i_websocketCleanup);
-    ms_apclient.start(i_apClientCount);
-    ms_otacheck.start(i_otaCheck);
-  }
-
-  #if defined(DEBUG_TASK_TO_CONSOLE)
-    // Get the stack high water mark for optimizing bytes allocated.
-    Serial.print(F("WiFiSetupTask Stack HWM: "));
-    Serial.println(uxTaskGetStackHighWaterMark(NULL));
-  #endif
-
-  // Task ends after setup is complete and MUST be removed from scheduling.
-  // Failure to do this can cause an error within the watchdog timer!
-  vTaskDelete(NULL);
-}
-
-void printPartitions() {
-  const esp_partition_t *partition;
-  esp_partition_iterator_t iterator = esp_partition_find(ESP_PARTITION_TYPE_ANY, ESP_PARTITION_SUBTYPE_ANY, NULL);
-
-  if (iterator == nullptr) {
-    Serial.println(F("No partitions found."));
-    return;
-  }
-
-  Serial.println(F("Partitions:"));
-  while (iterator != nullptr) {
-    partition = esp_partition_get(iterator);
-    Serial.printf("Label: %s, Size: %u bytes, Address: 0x%08X\n",
-                  partition->label,
-                  partition->size,
-                  partition->address);
-    iterator = esp_partition_next(iterator);
-  }
-
-  esp_partition_iterator_release(iterator);  // Release the iterator once done
-}
-
-void setup() {
-  Serial.begin(115200); // Serial monitor via USB connection.
-
-  // Expect a Serial2 connection with communication to a GPStar Proton Pack PCB.
-  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  packComs.begin(Serial2, false);
-
-  // Prepare the on-board (non-power) LED to be used as an output pin for indication.
-  pinMode(BUILT_IN_LED, OUTPUT);
-
-  // Provide an opportunity to set the CPU Frequency MHz: 80, 160, 240 [Default = 240]
-  // Lower frequency means less power consumption, but slower performance (obviously).
-  setCpuFrequencyMhz(240);
-  Serial.print(F("CPU Freq (MHz): "));
-  Serial.println(getCpuFrequencyMhz());
-
-  // Assume the Super Hero arming mode with Afterlife (default for Haslab).
-  SYSTEM_MODE = MODE_SUPER_HERO;
-  RED_SWITCH_MODE = SWITCH_OFF;
-  SYSTEM_YEAR = SYSTEM_AFTERLIFE;
-
-  // Boot into proton mode (default for pack and wand).
-  STREAM_MODE = PROTON;
-
-  // Set a default animation for the radiation indicator.
-  RAD_LENS_IDLE = AMBER_PULSE;
-  DISPLAY_TYPE = STATUS_TEXT;
-
-  // Begin at menu level one. This affects the behavior of the rotary dial.
-  MENU_LEVEL = MENU_1;
-
-  if(!b_wait_for_pack) {
-    // If not waiting for the pack set power level to 5.
-    POWER_LEVEL = LEVEL_5;
-  }
-  else {
-    // When waiting for the pack set power level to 1.
-    POWER_LEVEL = LEVEL_1;
-  }
-
-  // RGB LEDs for effects (upper/lower) and user status (top).
-  FastLED.addLeds<NEOPIXEL, DEVICE_LED_PIN>(device_leds, DEVICE_NUM_LEDS);
-
-  // Set all LEDs as off (black) until the device is ready.
-  device_leds[0] = getHueAsRGB(0, C_BLACK);
-  device_leds[1] = getHueAsRGB(1, C_BLACK);
-  device_leds[2] = getHueAsRGB(2, C_BLACK);
-
-  // Debounce the toggle switches and encoder pushbutton.
-  switch_left.setDebounceTime(switch_debounce_time);
-  switch_right.setDebounceTime(switch_debounce_time);
-  encoder_center.setDebounceTime(switch_debounce_time);
-
-  // Rotary encoder on the top of the Attenuator.
-  pinMode(r_encoderA, INPUT_PULLUP);
-  pinMode(r_encoderB, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(r_encoderA), readEncoder, CHANGE);
-
-  // Setup the bargraph after a brief delay.
-  delay(10);
-  setupBargraph();
-
-  // Feedback devices (piezo buzzer and vibration motor)
-  pinMode(BUZZER_PIN, OUTPUT);
-  setToneChannel(0); // Forces Tone to use Channel 0.
-
-  // Use the combined method for the arduino-esp32 platform, using the esp-idf v5.3+
-  ledcAttachChannel(VIBRATION_PIN, 5000, 8, 5); // Uses 5 kHz frequency, 8-bit resolution, channel 5
-
-  // Turn off any user feedback.
-  buzzOff();
-  vibrateOff();
-
-  // Get initial switch/button states.
-  switchLoops();
-
-  // Delay before configuring and running tasks.
-  delay(200);
-
-  // Initialize a critical timer for serial comms.
-  if(b_wait_for_pack) {
-    ms_packsync.start(0);
-  }
 
   // Print partition information to verify NVS availability
   #if defined(DEBUG_SEND_TO_CONSOLE)
@@ -471,6 +241,260 @@ void setup() {
     }
   }
 
+  #if defined(DEBUG_TASK_TO_CONSOLE)
+    // Get the stack high water mark for optimizing bytes allocated.
+    Serial.print(F("PreferencesTask Stack HWM: "));
+    Serial.println(uxTaskGetStackHighWaterMark(NULL));
+  #endif
+
+  // Task ends after setup is complete and MUST be removed from scheduling.
+  // Failure to do this can cause an error within the watchdog timer!
+  vTaskDelete(NULL);
+}
+
+// Serial Comms Task (Loop)
+void SerialCommsTask(void *parameter) {
+  #if defined(DEBUG_TASK_TO_CONSOLE)
+    // Confirm the core in use for this task, and when it runs.
+    Serial.print(F("Executing SerialCommsTask in core"));
+    Serial.print(xPortGetCoreID());
+    // Get the stack high water mark for optimizing bytes allocated.
+    Serial.print(F(" | Stack HWM: "));
+    Serial.println(uxTaskGetStackHighWaterMark(NULL));
+  #endif
+
+  while(true) {
+    if(b_wait_for_pack) {
+      if(ms_packsync.justFinished()) {
+        // Tell the pack we are trying to sync.
+        attenuatorSerialSend(A_SYNC_START);
+
+        // Keep the on-board LED dark until sync'd.
+        digitalWrite(BUILT_IN_LED, LOW);
+
+        // Pause and try again in a moment.
+        ms_packsync.start(i_sync_initial_delay);
+      }
+
+      checkPack();
+
+      if(!b_wait_for_pack) {
+        // Indicate that we are no longer waiting on the pack.
+        digitalWrite(BUILT_IN_LED, HIGH);
+      }
+    }
+    else {
+      bool b_notify = checkPack(); // Always updates on pack check.
+
+      // If at any point this flag is true, we have comms open to the pack.
+      // This gets reset upon every bootup (read: re-connection to a pack).
+      if(b_notify) {
+        b_comms_open = true;
+      }
+
+      if(ms_packsync.justFinished()) {
+        // The pack just went missing, so treat as disconnected.
+        b_wait_for_pack = true;
+        ms_packsync.start(i_sync_initial_delay);
+      }
+
+      /**
+       * Alert any WebSocket clients after an API call was received.
+       *
+       * Note: We only perform this action if we have data from the pack
+       * which resulted in a significant state change--this prevents the
+       * device from spamming any downstream clients with unchanged data.
+       */
+      if(b_notify) {
+        notifyWSClients(); // Send latest status to the WebSocket.
+      }
+    }
+
+    vTaskDelay(2 / portTICK_PERIOD_MS); // 2ms delay
+  }
+}
+
+// User Input Task (Loop)
+void UserInputTask(void *parameter) {
+  while(true) {
+    #if defined(DEBUG_TASK_TO_CONSOLE)
+      // Confirm the core in use for this task, and when it runs.
+      Serial.print(F("Executing UserInputTask in core"));
+      Serial.print(xPortGetCoreID());
+      // Get the stack high water mark for optimizing bytes allocated.
+      Serial.print(F(" | Stack HWM: "));
+      Serial.println(uxTaskGetStackHighWaterMark(NULL));
+    #endif
+
+    if(!b_wait_for_pack) {
+      // When not waiting for the pack go directly to checking user inputs.
+      checkUserInputs();
+    }
+
+    vTaskDelay(14 / portTICK_PERIOD_MS); // 14ms delay
+  }
+}
+
+// WiFi Management Task (Loop)
+void WiFiManagementTask(void *parameter) {
+  while(true) {
+    #if defined(DEBUG_TASK_TO_CONSOLE)
+      // Confirm the core in use for this task, and when it runs.
+      Serial.print(F("Executing WiFiManagementTask in core"));
+      Serial.print(xPortGetCoreID());
+      // Get the stack high water mark for optimizing bytes allocated.
+      Serial.print(F(" | Stack HWM: "));
+      Serial.println(uxTaskGetStackHighWaterMark(NULL));
+    #endif
+
+    // Proceed with management if the AP and web server are started.
+    if(b_ap_started && b_ws_started) {
+      if(ms_cleanup.remaining() < 1) {
+        // Clean up oldest WebSocket connections.
+        ws.cleanupClients();
+
+        // Restart timer for next cleanup action.
+        ms_cleanup.start(i_websocketCleanup);
+      }
+
+      if(ms_cleanup.remaining() < 1) {
+        // Update the current count of AP clients.
+        i_ap_client_count = WiFi.softAPgetStationNum();
+
+        // Restart timer for next count.
+        ms_apclient.start(i_apClientCount);
+      }
+
+      if(ms_cleanup.remaining() < 1) {
+        // Handles device reboot after an OTA update.
+        ElegantOTA.loop();
+
+        // Restart timer for next check.
+        ms_otacheck.start(i_otaCheck);
+      }
+    }
+
+    vTaskDelay(100 / portTICK_PERIOD_MS); // 100ms delay
+  }
+}
+
+// WiFi Setup Task (Single-Run)
+void WiFiSetupTask(void *parameter) {
+  #if defined(DEBUG_TASK_TO_CONSOLE)
+    // Confirm the core in use for this task, and when it runs.
+    Serial.print(F("Executing WiFiSetupTask in core"));
+    Serial.println(xPortGetCoreID());
+  #endif
+
+  // Begin by setting up WiFi as a prerequisite to all else.
+  if(startWiFi()) {
+    // Start the local web server.
+    startWebServer();
+
+    // Begin timer for remote client events.
+    ms_cleanup.start(i_websocketCleanup);
+    ms_apclient.start(i_apClientCount);
+    ms_otacheck.start(i_otaCheck);
+  }
+
+  #if defined(DEBUG_TASK_TO_CONSOLE)
+    // Get the stack high water mark for optimizing bytes allocated.
+    Serial.print(F("WiFiSetupTask Stack HWM: "));
+    Serial.println(uxTaskGetStackHighWaterMark(NULL));
+  #endif
+
+  // Task ends after setup is complete and MUST be removed from scheduling.
+  // Failure to do this can cause an error within the watchdog timer!
+  vTaskDelete(NULL);
+}
+
+void setup() {
+  Serial.begin(115200); // Serial monitor via USB connection.
+  delay(1000); // Provide a delay to allow serial output.
+
+  // Expect a Serial2 connection with communication to a GPStar Proton Pack PCB.
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  packComs.begin(Serial2, false);
+
+  // Prepare the on-board (non-power) LED to be used as an output pin for indication.
+  pinMode(BUILT_IN_LED, OUTPUT);
+
+  // Provide an opportunity to set the CPU Frequency MHz: 80, 160, 240 [Default = 240]
+  // Lower frequency means less power consumption, but slower performance (obviously).
+  setCpuFrequencyMhz(240);
+  #if defined(DEBUG_SEND_TO_CONSOLE)
+    Serial.print(F("CPU Freq (MHz): "));
+    Serial.println(getCpuFrequencyMhz());
+  #endif
+
+  // Assume the Super Hero arming mode with Afterlife (default for Haslab).
+  SYSTEM_MODE = MODE_SUPER_HERO;
+  RED_SWITCH_MODE = SWITCH_OFF;
+  SYSTEM_YEAR = SYSTEM_AFTERLIFE;
+
+  // Boot into proton mode (default for pack and wand).
+  STREAM_MODE = PROTON;
+
+  // Set a default animation for the radiation indicator.
+  RAD_LENS_IDLE = AMBER_PULSE;
+  DISPLAY_TYPE = STATUS_TEXT;
+
+  // Begin at menu level one. This affects the behavior of the rotary dial.
+  MENU_LEVEL = MENU_1;
+
+  if(!b_wait_for_pack) {
+    // If not waiting for the pack set power level to 5.
+    POWER_LEVEL = LEVEL_5;
+  }
+  else {
+    // When waiting for the pack set power level to 1.
+    POWER_LEVEL = LEVEL_1;
+  }
+
+  // RGB LEDs for effects (upper/lower) and user status (top).
+  FastLED.addLeds<NEOPIXEL, DEVICE_LED_PIN>(device_leds, DEVICE_NUM_LEDS);
+
+  // Set all LEDs as off (black) until the device is ready.
+  device_leds[0] = getHueAsRGB(0, C_BLACK);
+  device_leds[1] = getHueAsRGB(1, C_BLACK);
+  device_leds[2] = getHueAsRGB(2, C_BLACK);
+
+  // Debounce the toggle switches and encoder pushbutton.
+  switch_left.setDebounceTime(switch_debounce_time);
+  switch_right.setDebounceTime(switch_debounce_time);
+  encoder_center.setDebounceTime(switch_debounce_time);
+
+  // Rotary encoder on the top of the Attenuator.
+  pinMode(r_encoderA, INPUT_PULLUP);
+  pinMode(r_encoderB, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(r_encoderA), readEncoder, CHANGE);
+
+  // Setup the bargraph after a brief delay.
+  delay(10);
+  setupBargraph();
+
+  // Feedback devices (piezo buzzer and vibration motor)
+  pinMode(BUZZER_PIN, OUTPUT);
+  setToneChannel(0); // Forces Tone to use Channel 0.
+
+  // Use the combined method for the arduino-esp32 platform, using the esp-idf v5.3+
+  ledcAttachChannel(VIBRATION_PIN, 5000, 8, 5); // Uses 5 kHz frequency, 8-bit resolution, channel 5
+
+  // Turn off any user feedback.
+  buzzOff();
+  vibrateOff();
+
+  // Get initial switch/button states.
+  switchLoops();
+
+  // Delay before configuring and running tasks.
+  delay(200);
+
+  // Initialize a critical timer for serial comms.
+  if(b_wait_for_pack) {
+    ms_packsync.start(0);
+  }
+
   /**
    * By default the WiFi will run on core0, while the standard loop() runs on core1.
    * We can make efficient use of the available cores by "pinning" a task to a core.
@@ -488,6 +512,12 @@ void setup() {
    *  Task Handle Reference,
    *  Pinned Core (0 or 1)
    */
+
+  // Create a single-run setup task with the highest priority for WiFi/WebServer startup.
+  xTaskCreatePinnedToCore(PreferencesTask, "PreferencesTask", 4096, NULL, 6, &PreferencesTaskHandle, 1);
+
+  // Delay all lower priority tasks until Preferences are loaded.
+  vTaskDelay(100 / portTICK_PERIOD_MS); // Delay for 100ms to avoid competition.
 
   // Create a single-run setup task with the highest priority for WiFi/WebServer startup.
   xTaskCreatePinnedToCore(WiFiSetupTask, "WiFiSetupTask", 4096, NULL, 5, &WiFiSetupTaskHandle, 1);
