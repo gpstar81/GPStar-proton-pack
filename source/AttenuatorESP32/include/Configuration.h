1f30f0b3f7a8ff43b56c60b6a783c3438dc10d25
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-11-18 07:18:21 -0500

    Sync whitespace changes/prefs, rebuild binaries

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -45,7 +45,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20241117201418";
+String build_date = "20241118071744";
 
 /*
  * Preferred WiFi Network Defaults
