c218d1af8edc689233b4f5fd4fab85fa43d1ece4
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-11-17 20:41:38 -0500

    Adjust libraries, rebuild

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -45,7 +45,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20241117200304";
+String build_date = "20241117203812";
 
 /*
  * Preferred WiFi Network Defaults
