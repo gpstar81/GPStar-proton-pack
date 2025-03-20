0a9283128fd456c7a30b9210368700d73ac782a4
nomakewan <nomakewan@users.noreply.github.com>
2025-03-20 15:01:18 -0700

    Update ESP32 libraries
    Updates ESPAsyncWebServer to 3.7.4, updates ElegantOTA to 3.1.7.

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -45,7 +45,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250317230618";
+String build_date = "20250320145718";
 
 /*
  * Preferred WiFi Network Defaults
