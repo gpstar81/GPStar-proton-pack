f1b8f8e07a07b3dcbe62c93d2718b71eb2b75d48
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-05-01 06:27:38 -0400

    ESP32 Crash Fix (#432)
    * Fix FastLED crash
    
    Fixes core abort caused by FastLED animation task smashing the stack by doubling the stack size for that task.
    
    * Additional ESP32 fixes
    
    Adds the stack size fix to the BeltGizmo and StreamEffects devices as well.
    
    ---------
    
    Co-authored-by: nomakewan <nomakewan@users.noreply.github.com>

--- source/StreamEffects/include/Configuration.h
+++ source/StreamEffects/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250430063907";
+String build_date = "20250430200748";
 
 /*
  * Parent WiFi Network Defaults
