da922609451cdeba2483dc4fdc1fda4cd8c487ed
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-05-08 10:42:24 -0400

    Rebuild all binaries

--- source/StreamEffects/include/Configuration.h
+++ source/StreamEffects/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250430200748";
+String build_date = "20250508104058";
 
 /*
  * Parent WiFi Network Defaults
