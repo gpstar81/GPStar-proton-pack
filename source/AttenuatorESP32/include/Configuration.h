dd0e7cdce3dacc5e29957803c43ee8fcb790b2a3
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-05-22 06:49:05 -0400

    Recompile binaries

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -45,7 +45,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250508104025";
+String build_date = "V6_20250522064758";
 
 /*
  * Preferred WiFi Network Defaults
