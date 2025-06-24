6d5e34941466d0c0f579498c990225af5a344f62
nomakewan <nomakewan@users.noreply.github.com>
2025-06-24 02:31:43 -0700

    FastLED 3.10.1 update
    Updates to FastLED 3.10.1, which resolves some potential issues on the ESP32 builds.

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -45,7 +45,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250620023036";
+String build_date = "V6_20250624022601";
 
 /*
  * Preferred WiFi Network Defaults
