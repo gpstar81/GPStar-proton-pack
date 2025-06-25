a52ec4750842ec9cdfafae255d8eaa41b0f949aa
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-06-25 10:05:48 -0400

    Begin support for multi-environment compile using PIO

--- .github/compile_pack.sh
+++ .github/compile_pack.sh
@@ -12,23 +12,33 @@ mkdir -p ${BINDIR}/pack
 echo ""
 
 # Proton Pack
-echo "Building Proton Pack Binary..."
+echo "Building Proton Pack Binary [ATMega]..."
 
 # Set the project directory based on the source folder
 PROJECT_DIR="$SRCDIR/ProtonPack"
 
-#if [ -f ${BINDIR}/ProtonPack.ino.hex ]; then
-#  mv ${BINDIR}/ProtonPack.ino.hex ${BINDIR}/pack/ProtonPack.hex
-#fi
-
 # Clean the project before building
-pio run --project-dir "$PROJECT_DIR" --target clean
+pio run -e atmega2560 --project-dir "$PROJECT_DIR" --target clean
 
 # Compile the PlatformIO project
-pio run --project-dir "$PROJECT_DIR" | grep -iv Retrieved
+pio run -e atmega2560 --project-dir "$PROJECT_DIR" | grep -iv Retrieved
 
 if [ -f ${PROJECT_DIR}/.pio/build/megaatmega2560/firmware.hex ]; then
   mv ${PROJECT_DIR}/.pio/build/megaatmega2560/firmware.hex ${BINDIR}/pack/ProtonPack.hex
+fi
+echo "Done."
+echo ""
+
+echo "Building Proton Pack Binary [ESP32]..."
+
+# Clean the project before building
+#pio run -e esp32s3 --project-dir "$PROJECT_DIR" --target clean
+
+# Compile the PlatformIO project
+#pio run -e esp32s3 --project-dir "$PROJECT_DIR" | grep -iv Retrieved
+
+if [ -f ${PROJECT_DIR}/.pio/build/esp32s3dev/firmware.hex ]; then
+  mv ${PROJECT_DIR}/.pio/build/esp32s3dev/firmware.hex ${BINDIR}/pack/ProtonPack-ESP32.hex
 fi
 echo "Done."
 echo ""
--- .github/compile_wand.sh
+++ .github/compile_wand.sh
@@ -12,48 +12,74 @@ mkdir -p ${BINDIR}/wand/extras
 echo ""
 
 # Neutrona Wand
-echo "Building Neutrona Wand Binary..."
+echo "Building Neutrona Wand Binary [ATMega]..."
 
 # Set the project directory based on the source folder
 PROJECT_DIR="$SRCDIR/NeutronaWand"
 
-#if [ -f ${BINDIR}/NeutronaWand.ino.hex ]; then
-#  mv ${BINDIR}/NeutronaWand.ino.hex ${BINDIR}/wand/NeutronaWand.hex
-#fi
-
 # Clean the project before building
-pio run --project-dir "$PROJECT_DIR" --target clean
+pio run -e atmega2560 --project-dir "$PROJECT_DIR" --target clean
 
 # Compile the PlatformIO project
-pio run --project-dir "$PROJECT_DIR" | grep -iv Retrieved
+pio run -e atmega2560 --project-dir "$PROJECT_DIR" | grep -iv Retrieved
 
-if [ -f ${PROJECT_DIR}/.pio/build/megaatmega2560/firmware.hex ]; then
-  mv ${PROJECT_DIR}/.pio/build/megaatmega2560/firmware.hex ${BINDIR}/wand/NeutronaWand.hex
+if [ -f ${PROJECT_DIR}/.pio/build/atmega2560/firmware.hex ]; then
+  mv ${PROJECT_DIR}/.pio/build/atmega2560/firmware.hex ${BINDIR}/wand/NeutronaWand.hex
+fi
+echo "Done."
+echo ""
+
+echo "Building Neutrona Wand Binary [ESP32]..."
+
+# Clean the project before building
+#pio run -e esp32s3 --project-dir "$PROJECT_DIR" --target clean
+
+# Compile the PlatformIO project
+#pio run -e esp32s3 --project-dir "$PROJECT_DIR" | grep -iv Retrieved
+
+if [ -f ${PROJECT_DIR}/.pio/build/esp32s3/firmware.hex ]; then
+  mv ${PROJECT_DIR}/.pio/build/esp32s3/firmware.hex ${BINDIR}/wand/NeutronaWand-ESP32.hex
 fi
 echo "Done."
 echo ""
 
 # Neutrona Wand (Bench Test)
-echo "Building Neutrona Wand (Bench Test) Binary..."
+echo "Building Neutrona Wand (Bench Test) Binary [ATMega]..."
 
 # Change flag(s) for compilation
 sed -i -e 's/bool b_gpstar_benchtest = false/const bool b_gpstar_benchtest = true/' ${PROJECT_DIR}/include/Configuration.h
 sed -i -e 's/b_gpstar_benchtest = true/\/\/b_gpstar_benchtest = true/' ${PROJECT_DIR}/include/Serial.h
 
 # Clean the project before building
-pio run --project-dir "$PROJECT_DIR" --target clean
+pio run -e atmega2560 --project-dir "$PROJECT_DIR" --target clean
 
 # Compile the PlatformIO project
-pio run --project-dir "$PROJECT_DIR" | grep -iv Retrieved
+pio run -e atmega2560 --project-dir "$PROJECT_DIR" | grep -iv Retrieved
+
+rm -f ${PROJECT_DIR}/include/*.h-e
+
+if [ -f ${PROJECT_DIR}/.pio/build/atmega2560/firmware.hex ]; then
+  mv ${PROJECT_DIR}/.pio/build/atmega2560/firmware.hex ${BINDIR}/wand/extras/NeutronaWand-BenchTest.hex
+fi
+echo "Done."
+echo ""
+
+echo "Building Neutrona Wand (Bench Test) Binary [ESP32]..."
+
+# Clean the project before building
+#pio run -e esp32s3 --project-dir "$PROJECT_DIR" --target clean
+
+# Compile the PlatformIO project
+#pio run -e esp32s3 --project-dir "$PROJECT_DIR" | grep -iv Retrieved
+
+rm -f ${PROJECT_DIR}/include/*.h-e
+
+if [ -f ${PROJECT_DIR}/.pio/build/esp32s3/firmware.hex ]; then
+  mv ${PROJECT_DIR}/.pio/build/esp32s3/firmware.hex ${BINDIR}/wand/extras/NeutronaWand-BenchTest-ESP32.hex
+fi
+echo "Done."
+echo ""
 
 # Restore flag(s) from compilation
 sed -i -e 's/const bool b_gpstar_benchtest = true/bool b_gpstar_benchtest = false/' ${PROJECT_DIR}/include/Configuration.h
 sed -i -e 's/\/\/b_gpstar_benchtest = true/b_gpstar_benchtest = true/' ${PROJECT_DIR}/include/Serial.h
-
-rm -f ${PROJECT_DIR}/include/*.h-e
-
-if [ -f ${PROJECT_DIR}/.pio/build/megaatmega2560/firmware.hex ]; then
-  mv ${PROJECT_DIR}/.pio/build/megaatmega2560/firmware.hex ${BINDIR}/wand/extras/NeutronaWand-BenchTest.hex
-fi
-echo "Done."
-echo ""
