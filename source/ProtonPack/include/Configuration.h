6d77605db563f0500ff6f88c47f8a9fd6ff7e9c7
nomakewan <nomakewan@users.noreply.github.com>
2025-07-30 12:36:25 -0700

    Update ESP32 framework, libraries
    Updates ESP32 to Arduino 3.3.0/ESP-IDF 5.5.0, updates ArduinoJson to 7.4.2, updates LSM6DS3 to use PlatformIO registry.

--- source/ProtonPack/include/Configuration.h
+++ source/ProtonPack/include/Configuration.h
@@ -22,7 +22,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250729105516";
+String build_date = "V6_20250730121733";
 
 /*
  * Preferred WiFi Network Defaults (only for ESP32)
