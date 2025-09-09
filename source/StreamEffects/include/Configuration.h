1b115a0a823f76ed40869b24ca19dd29a48c081f
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-09-09 12:15:03 -0400

    Update firmware for stream effects

--- source/StreamEffects/include/Configuration.h
+++ source/StreamEffects/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250909080948";
+String build_date = "V6_20250909121230";
 
 /*
  * Parent WiFi Network Defaults
