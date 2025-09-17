bf3cc49811b548dfa2e770e3187815d58491cf58
nomakewan <nomakewan@users.noreply.github.com>
2025-09-17 04:56:27 -0700

    Update ESPAsyncWebServer
    Updates ESPAsyncWebServer to 3.8.1

--- source/SingleShot/include/Configuration.h
+++ source/SingleShot/include/Configuration.h
@@ -24,7 +24,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250917030625";
+String build_date = "V6_20250917045010";
 
 /*
  * Preferred WiFi Network Defaults (only for ESP32)
