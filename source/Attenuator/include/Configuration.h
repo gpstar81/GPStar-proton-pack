865408e4cc50a8484ca636015ceb4a8a0762ac8f
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-09-25 08:24:12 -0400

    Full recompile

--- source/Attenuator/include/Configuration.h
+++ source/Attenuator/include/Configuration.h
@@ -47,7 +47,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250925012715";
+String build_date = "V6_20250925081904";
 
 /*
  * Preferred WiFi Network Defaults
--- source/BeltGizmo/include/Configuration.h
+++ source/BeltGizmo/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250925012715";
+String build_date = "V6_20250925081904";
 
 /*
  * Parent WiFi Network Defaults
