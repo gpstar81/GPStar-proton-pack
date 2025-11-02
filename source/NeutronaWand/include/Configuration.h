7940492b7d450f2c4d31d51c1d3bc3a505148802
nomakewan <nomakewan@users.noreply.github.com>
2025-11-02 01:02:58 -0800

    Remove erroneous header comment
    Removes erroneous header comment from password.html on the trap

--- source/NeutronaWand/include/Configuration.h
+++ source/NeutronaWand/include/Configuration.h
@@ -23,7 +23,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20251102005704";
+String build_date = "V6_20251102015525";
 
 /*
  * Control debug messages for various actions during normal operation.
