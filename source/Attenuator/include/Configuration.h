feeedd695c411e85d23687a9ed6187ef5e5aa59a
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-11-12 16:08:29 -0500

    Full recompile

--- source/Attenuator/include/Configuration.h
+++ source/Attenuator/include/Configuration.h
@@ -47,7 +47,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20251112041121";
+String build_date = "V6_20251112151244";
 
 /*
  * Enable Physical Feedback Effects (Sound + Vibration)
--- source/StreamEffects/include/Configuration.h
+++ source/StreamEffects/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20251112041121";
+String build_date = "V6_20251112151244";
 
 /*
  * Custom values from pack EEPROM.
